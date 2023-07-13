f28bfbd6f579d8e8890a9ad6f58ae7c62be008b1
Amer-xyz <amer27033@gmail.com>
2023-07-13 02:52:33 -0500

    Custom Keymap

--- keyboards/sofle/keymaps/Amer-xyz/config.h
+++ keyboards/sofle/keymaps/Amer-xyz/config.h
@@ -1,4 +1,7 @@
 #pragma once
+#define FORCE_NKRO
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define MASTER_LEFT
 
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
